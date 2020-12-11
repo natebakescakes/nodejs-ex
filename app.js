6e2f4fae04d1177c1c4bcb7ecdec53f2129d354a
Nathan Khoo <natebakescakes@gmail.com>
2020-12-11 17:48:13 +0800

    add new endpoint

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const axios = require('axios');
 
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, openshift workshop');
+});
+
+app.get('/friendship/bracelet', async (req, res) => {
+  const response = await axios.get(
+    `${process.env.FRIENDSHIP_HOST}:${process.env.FRIENDSHIP_PORT}/api/friend`
+);
+  res.send(
+    `${response.data.name} is now your friend. They live at ${response.data.address}`
+);
 });
 
 module.exports = app;
